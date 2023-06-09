cce396e61d4d5da93dd2fdaf171361e9c3e6e3c4
Saidur Rahman <saidurhere@gmail.com>
2023-06-09 12:46:16 +0600

    Done with the splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import Splash from "@/components/splash/Splash";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-center text-6xl font-extrabold mt-12 text-white">
-        Hello world!
-      </h1>
+      <Splash></Splash>
     </main>
   );
 }
---
+++ src/components/splash/Splash.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const Splash = () => {
+  const greetingMsgs = [
+    "Hello",
+    "হ্যালো",
+    "مرحبا",
+    "नमस्ते",
+    "你好",
+    "こんにちは ",
+    "Olá",
+    "Ciao",
+    "Bonjour",
+    "안녕하세요 ",
+    "Привет  ",
+    " Γεια σας ",
+    " Cześć ",
+    "Hola",
+    "Halo",
+  ];
+  const [currentMsgIndex, setCurrentMsgIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFadeIn(false); // Set fadeIn to false after 100ms
+    }, 500);
+    const timerId = setTimeout(() => {
+      const intervalId = setInterval(() => {
+        setCurrentMsgIndex((prevIndex) => {
+          const nextIndex = prevIndex + 1;
+          if (nextIndex >= greetingMsgs.length - 1) {
+            clearInterval(intervalId);
+          }
+          return nextIndex % greetingMsgs.length;
+        });
+      }, 200);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [greetingMsgs.length]);
+
+  const currentMsg = greetingMsgs[currentMsgIndex];
+
+  return (
+    <div className="h-[100vh] bg-accent flex items-center justify-between flex-col py-[3%]">
+      <p
+        className={`transition-opacity duration-500 ease-in-out text-white text-5xl lg:text-8xl lg:mt-[32vh] mt-[36vh] ${
+          fadeIn ? "opacity-0" : ""
+        }
+         `}
+      >
+        <span>•</span> <span>{currentMsg}</span>
+      </p>
+      <div className="loader scale-75 lg:scale-100">
+        <div className="box1" />
+        <div className="box2" />
+        <div className="box3" />
+      </div>
+    </div>
+  );
+};
+
+export default Splash;
